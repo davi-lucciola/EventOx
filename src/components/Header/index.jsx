f33a0a16a43287a4486f2e012ca3627bfe8af68a
Davi <davilucciola14@gmail.com>
2023-06-03 16:38:24 -0300

    feat: first commit

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+import EventOxLogo from './../../assets/event-ox-icon.png';
+
+
+export const Header = () => {
+    return (
+        <header>
+            <a href="">
+                <img src={EventOxLogo}/>
+            </a>
+        </header>
+    )
+}
